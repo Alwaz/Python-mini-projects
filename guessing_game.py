ea0506928d14183284bf09ff9e6d2ab3020142ca
Alwaz <alwazkazi3@gmail.com>
2021-08-25 08:17:08 -0700

    making computer guess the number

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,7 @@
 import random
 
+
+# we will guess
 def guess(ending_range):
     randome_range=random.randint(1,ending_range)
     guess = 0
@@ -9,9 +11,26 @@ def guess(ending_range):
             print("Sorry! try again guess id too high")
         elif guess<randome_range:
             print("Sorry wrong guess it is too low")
-
-
-
     print("yay! you havr guessed the number "+ str(randome_range) + " correctly")
 
-print(guess(50))
+
+
+# Computer will guess here
+def computer_guess(ending_range):
+    low = 1
+    high = ending_range
+    feedback= ""
+
+
+
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input("Is the number "+ str(guess) + " is too high (h), too low(l) or is it correct (c) ")
+        if feedback == 'h':
+            high=guess
+        elif feedback == 'l':
+            low=guess+1
+    print("Thank God I finally guessed it")
+
+
+computer_guess(10)
