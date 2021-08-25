ad3671a44d74eecc2e9d462f41718a56404124b9
Alwaz <alwazkazi3@gmail.com>
2021-08-25 06:55:59 -0700

    basic Madlipz game

---
+++ main.py
@@ -0,0 +1,8 @@
+noun = input("Please Enter a Noun: ")
+verb = input("Please enter a verb here ")
+celebrity_name=input("Enter a Celebrity name here: ")
+
+
+paragraph = "Today I was  going for a " + verb + " but i encountered a " + noun + " So I stayed back and watched the movies of "+celebrity_name
+
+print(paragraph)
